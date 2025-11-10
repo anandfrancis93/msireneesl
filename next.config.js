1fc549d8295691a4dcd225b45663ec8ed323332e
Anand Francis <anandfrancis93@users.noreply.github.com>
2025-11-10 10:53:14 -0700

    Fix CMS config.yml serving with proper headers

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  async headers() {
+    return [
+      {
+        source: '/admin/config.yml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/yaml',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
